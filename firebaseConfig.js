14055f0c6fe14bd291aa2b7f65408b75fd0956f5
Lionandthelab <lionandthelab@gmail.com>
2021-10-31 00:38:24 +0900

    firebaseConfig.js
    Signed-off-by: Lionandthelab <lionandthelab@gmail.com>

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+}
