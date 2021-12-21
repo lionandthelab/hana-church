a1e1bc034ed908eab112f31d68552ba5261a4984
Lionandthelab <lionandthelab@gmail.com>
2021-12-22 01:11:43 +0900

    new icon, read thru UX
    Signed-off-by: Lionandthelab <lionandthelab@gmail.com>

--- src/pages/ReadThru.vue
+++ src/pages/ReadThru.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import ReadThruView from 'src/components/ReadThruView.vue';
 import { firebaseUser } from 'src/composables/useAuth';
-import { ref, watchEffect } from 'vue';
+import { ref, watchEffect, onUpdated } from 'vue';
 import { db } from 'boot/firebase';
 import {
   setDoc,
@@ -112,6 +112,7 @@ const registerUserInfo = () => {
     .then(() => console.log('registerUserInfo'))
     .catch((e) => console.log('registerUserInfo errr -', e));
 };
+
 //firebase functions
 //update data & initialize
 const checkInit = ref(false); //check firebase data set
@@ -127,6 +128,8 @@ watchEffect(() => {
     } else registerUserInfo();
   }
 });
+
+onUpdated(() => updateProxy());
 </script>
 <template>
   <q-page-container style="padding: 0px">
