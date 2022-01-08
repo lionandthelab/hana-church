75a89cbe15367a987e09bc9968a2d15d3a545bcd
Euiseok Kim <ikess0330@snu.ac.kr>
2022-01-08 22:07:15 +0900

    new commutiny page
    Signed-off-by: Euiseok Kim <ikess0330@snu.ac.kr>

--- src/pages/Community.vue
+++ src/pages/Community.vue
@@ -1,18 +1,63 @@
 <script setup lang="ts">
-import {} from 'vue';
-import { useRouter } from 'vue-router';
-import { fasBookmark } from '@quasar/extras/fontawesome-v5';
-
-const router = useRouter();
-const gotoUCM = async () => {
-  await router.push('/message/UCM');
-};
+const communities = [
+  {
+    title: '조이랜드',
+    who: '미취학, 유/초/중/고등부 학생',
+    contactTo: ['010-3539-0563', '(설예리 집사)'],
+    pictureURL: require('assets/조이랜드.jpeg'),
+  },
+  {
+    title: '조이스쿨',
+    who: '미취학, 유/초등부 학생',
+    contactTo: ['010-4775-3627', '(이지웅 전도사)'],
+    pictureURL: require('assets/조이스쿨.jpeg'),
+  },
+  {
+    title: 'UCM (대학부)',
+    who: '대학생',
+    contactTo: ['010-4775-3627', '(이지웅 전도사)'],
+    pictureURL: require('assets/UCM.jpeg'),
+  },
+  {
+    title: '청년부',
+    who: 'B존 이전의 청년',
+    contactTo: ['010-4775-3627', '(이지웅 전도사)'],
+    pictureURL: require('assets/청년부.png'),
+  },
+  {
+    title: 'B존',
+    who: '',
+    contactTo: ['010-7559-0563', '(황도경 집사)'],
+    pictureURL: require('assets/B존.jpeg'),
+  },
+  {
+    title: 'C존',
+    who: '',
+    contactTo: ['010-4775-3627', '(이지웅 전도사)'],
+    pictureURL: require('assets/C존.jpeg'),
+  },
+  {
+    title: 'D존',
+    who: '',
+    contactTo: ['010-4775-3627', '(이지웅 전도사)'],
+    pictureURL: require('assets/D존.jpeg'),
+  },
+  {
+    title: 'SHEM (영어예배)',
+    who: '한국에 있는 영어권 청년',
+    contactTo: ['010-4946-8797', '(이승완 형제)'],
+    pictureURL: require('assets/SHEM.png'),
+  },
+  {
+    title: '중국어예배',
+    who: '한국에 있는 중국어권 청년',
+    contactTo: ['010-7119-8853', '(강미령 집사)'],
+    pictureURL: require('assets/중국어예배.jpeg'),
+  },
+];
 </script>
 <template>
-  <q-page
-    style="padding-top: 50px; width: 100%; height: 100%; overflow"
-    :class="$q.dark.isActive ? 'bg-dark' : 'bg-white'"
-  >
+  <q-page style="padding-top: 50px; width: 100%; height: 100vh; overflow">
     <q-page-sticky expand position="top" style="z-index: 999">
       <q-toolbar class="bg-primary text-white" style="border: 1px solid">
         <q-avatar>
@@ -24,165 +69,34 @@ const gotoUCM = async () => {
     </q-page-sticky>
 
     <div class="q-pb-md q-gutter-md">
-      <div class="row text-center justify-center">
-        <q-parallax src="../assets/hana-church-logo.png" :height="200">
-          <div class="column full-width align-center">
-            <div class="q-px-lg text-primary text-weight-bolder text-h5">
-              사랑과 성령으로 충만한
-            </div>
-            <div class="q-px-lg text-primary text-weight-bolder text-h5">
-              제자들을 낳고 키우고 보내는
-            </div>
-            <div class="q-pa-md text-primary text-weight-bolder text-h4">
-              초대교회 같은 공동체
-            </div>
-          </div>
-        </q-parallax>
-      </div>
+      <div class="row justify-between"></div>
 
       <div class="row q-pa-md">
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-          <q-card class="q-ma-md my-card">
-            <q-img src="../assets/UCM토요예배15시.png" />
-
-            <q-card-section>
-              <q-btn
-                fab
-                color="primary"
-                :icon="fasBookmark"
-                class="absolute"
-                style="top: 0; right: 12px; transform: translateY(-50%)"
-                @click="gotoUCM()"
-                >예배 바로가기</q-btn
-              >
-              <div class="row no-wrap items-center">
-                <div class="col text-h6 ellipsis">UCM</div>
+        <div
+          v-for="i in communities.length"
+          :key="i"
+          class="q-pa-md col-xs-12 col-sm-6 col-md-6 col-lg-4"
+        >
+          <q-card class="my-card fit">
+            <q-img class="fit" :src="communities[i - 1].pictureURL">
+              <div class="text-h5 absolute-bottom text-right">
                 <div
-                  class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
-                ></div>
+                  class="row text-caption no-wrap items-center q-pa-none justify-end"
+                >
+                  <q-btn flat round icon="phone" />
+                  {{ communities[i - 1].contactTo[0] }}
+                  {{ communities[i - 1].contactTo[1] }}
+                </div>
+                <div class="row no-wrap items-center justify-end">
+                  <div class="q-pr-sm text-caption">
+                    {{ communities[i - 1].who }}
+                  </div>
+                  <div class="text-weight-bolder text-h6 ellipsis">
+                    {{ communities[i - 1].title }}
+                  </div>
+                </div>
               </div>
-            </q-card-section>
-
-            <q-card-section class="q-pt-none">
-              <div class="text-subtitle1">구성원</div>
-              <div class="text-caption text-grey">대학생, 사회 초년생</div>
-            </q-card-section>
-
-            <q-separator />
-
-            <q-card-actions>
-              <q-btn flat round icon="phone" />
-              010-4775-3627 (이지웅 전도사)
-            </q-card-actions>
-          </q-card>
-        </div>
-
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-          <q-card class="q-ma-md my-card">
-            <q-img src="../assets/조이랜드.png" />
-
-            <q-card-section>
-              <!-- <q-btn
-                fab
-                color="primary"
-                icon="person"
-                class="absolute"
-                style="top: 0; right: 12px; transform: translateY(-50%)"
-              >
-              </q-btn> -->
-
-              <div class="row no-wrap items-center">
-                <div class="col text-h6 ellipsis">조이랜드</div>
-                <div
-                  class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
-                ></div>
-              </div>
-            </q-card-section>
-
-            <q-card-section class="q-pt-none">
-              <div class="text-subtitle1">구성원</div>
-              <div class="text-caption text-grey">미취학, 유/초등부 학생</div>
-            </q-card-section>
-
-            <q-separator />
-
-            <q-card-actions>
-              <q-btn flat round icon="phone" />
-              010-3539-0563 (설예리 집사)
-            </q-card-actions>
-          </q-card>
-        </div>
-
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-          <q-card class="q-ma-md my-card">
-            <q-img fit="scale-down" src="../assets/hana-church-logo-pad.png" />
-
-            <q-card-section>
-              <!-- <q-btn
-                fab
-                color="primary"
-                icon="person"
-                class="absolute"
-                style="top: 0; right: 12px; transform: translateY(-50%)"
-              >              
-              </q-btn> -->
-
-              <div class="row no-wrap items-center">
-                <div class="col text-h6 ellipsis">중국어 예배</div>
-                <div
-                  class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
-                ></div>
-              </div>
-            </q-card-section>
-
-            <q-card-section class="q-pt-none">
-              <div class="text-subtitle1">구성원</div>
-              <div class="text-caption text-grey">
-                한국에 있는 중국어권 청년
-              </div>
-            </q-card-section>
-
-            <q-separator />
-
-            <q-card-actions>
-              <q-btn flat round icon="phone" />
-              010-7119-8853 (강미령 집사)
-            </q-card-actions>
-          </q-card>
-        </div>
-
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-          <q-card class="q-ma-md my-card">
-            <q-img src="../assets/SHEM.png" />
-
-            <q-card-section>
-              <!-- <q-btn
-                fab
-                color="primary"
-                icon="person"
-                class="absolute"
-                style="top: 0; right: 12px; transform: translateY(-50%)"
-              /> -->
-
-              <div class="row no-wrap items-center">
-                <div class="col text-h6 ellipsis">영어 예배</div>
-                <div
-                  class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
-                ></div>
-              </div>
-            </q-card-section>
-
-            <q-card-section class="q-pt-none">
-              <div class="text-subtitle1">구성원</div>
-              <div class="text-caption text-grey">한국에 있는 영어권 청년</div>
-            </q-card-section>
-
-            <q-separator />
-
-            <q-card-actions>
-              <q-btn flat round icon="phone" />
-              010-4946-8797 (이승완 형제)
-            </q-card-actions>
+            </q-img>
           </q-card>
         </div>
       </div>
